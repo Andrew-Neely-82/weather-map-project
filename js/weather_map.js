8eeaefa52edcf0c1667111165be83cdc2c9b1996
Andrew-Neely-82 <andrew.neely82@gmail.com>
2022-12-01 16:16:57 -0600

    updated

--- js/weather_map.js
+++ js/weather_map.js
@@ -2,9 +2,43 @@
   `use strict`;
 
   // * TODO: Using HTML, CSS, jQuery, AJAX, and the OpenWeatherMap API, start by showing the current conditions for city you live in on your page.
-  $.get("http://api.openweathermap.org/data/2.5/weather", {
+  $.get("https://api.openweathermap.org/data/2.5/weather?lat=57&lon=-2.151", {
     APPID: weatherKey,
     q: "Fort Bragg, US",
+    units: "imperial",
+  }).done(function (data) {
+    console.log(data);
+    console.log("A step further - information for tomorrow: ", data.weather[0].description);
+    $(`.container-1-1`).append(`
+    <div class="container-1-1">
+      <span>
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-thermometer-half" viewBox="0 0 16 16">
+          <path d="M9.5 12.5a1.5 1.5 0 1 1-2-1.415V6.5a.5.5 0 0 1 1 0v4.585a1.5 1.5 0 0 1 1 1.415z"/>
+          <path d="M5.5 2.5a2.5 2.5 0 0 1 5 0v7.55a3.5 3.5 0 1 1-5 0V2.5zM8 1a1.5 1.5 0 0 0-1.5 1.5v7.987l-.167.15a2.5 2.5 0 1 0 3.333 0l-.166-.15V2.5A1.5 1.5 0 0 0 8 1z"/>
+        </svg>
+        ${data.main.temp}°F
+      </span>
+      <span>
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cloud-sun" viewBox="0 0 16 16">
+        <path d="M7 8a3.5 3.5 0 0 1 3.5 3.555.5.5 0 0 0 .624.492A1.503 1.503 0 0 1 13 13.5a1.5 1.5 0 0 1-1.5 1.5H3a2 2 0 1 1 .1-3.998.5.5 0 0 0 .51-.375A3.502 3.502 0 0 1 7 8zm4.473 3a4.5 4.5 0 0 0-8.72-.99A3 3 0 0 0 3 16h8.5a2.5 2.5 0 0 0 0-5h-.027z"/>
+        <path d="M10.5 1.5a.5.5 0 0 0-1 0v1a.5.5 0 0 0 1 0v-1zm3.743 1.964a.5.5 0 1 0-.707-.707l-.708.707a.5.5 0 0 0 .708.708l.707-.708zm-7.779-.707a.5.5 0 0 0-.707.707l.707.708a.5.5 0 1 0 .708-.708l-.708-.707zm1.734 3.374a2 2 0 1 1 3.296 2.198c.199.281.372.582.516.898a3 3 0 1 0-4.84-3.225c.352.011.696.055 1.028.129zm4.484 4.074c.6.215 1.125.59 1.522 1.072a.5.5 0 0 0 .039-.742l-.707-.707a.5.5 0 0 0-.854.377zM14.5 6.5a.5.5 0 0 0 0 1h1a.5.5 0 0 0 0-1h-1z"/>
+      </svg>
+      ${data.weather[0].description}
+    </span>
+    <span>
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-moisture" viewBox="0 0 16 16">
+        <path d="M13.5 0a.5.5 0 0 0 0 1H15v2.75h-.5a.5.5 0 0 0 0 1h.5V7.5h-1.5a.5.5 0 0 0 0 1H15v2.75h-.5a.5.5 0 0 0 0 1h.5V15h-1.5a.5.5 0 0 0 0 1h2a.5.5 0 0 0 .5-.5V.5a.5.5 0 0 0-.5-.5h-2zM7 1.5l.364-.343a.5.5 0 0 0-.728 0l-.002.002-.006.007-.022.023-.08.088a28.458 28.458 0 0 0-1.274 1.517c-.769.983-1.714 2.325-2.385 3.727C2.368 7.564 2 8.682 2 9.733 2 12.614 4.212 15 7 15s5-2.386 5-5.267c0-1.05-.368-2.169-.867-3.212-.671-1.402-1.616-2.744-2.385-3.727a28.458 28.458 0 0 0-1.354-1.605l-.022-.023-.006-.007-.002-.001L7 1.5zm0 0-.364-.343L7 1.5zm-.016.766L7 2.247l.016.019c.24.274.572.667.944 1.144.611.781 1.32 1.776 1.901 2.827H4.14c.58-1.051 1.29-2.046 1.9-2.827.373-.477.706-.87.945-1.144zM3 9.733c0-.755.244-1.612.638-2.496h6.724c.395.884.638 1.741.638 2.496C11 12.117 9.182 14 7 14s-4-1.883-4-4.267z"/>
+      </svg> 
+      ${data.main.humidity}%
+    </span>
+    <span>
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-wind" viewBox="0 0 16 16">
+        <path d="M12.5 2A2.5 2.5 0 0 0 10 4.5a.5.5 0 0 1-1 0A3.5 3.5 0 1 1 12.5 8H.5a.5.5 0 0 1 0-1h12a2.5 2.5 0 0 0 0-5zm-7 1a1 1 0 0 0-1 1 .5.5 0 0 1-1 0 2 2 0 1 1 2 2h-5a.5.5 0 0 1 0-1h5a1 1 0 0 0 0-2zM0 9.5A.5.5 0 0 1 .5 9h10.042a3 3 0 1 1-3 3 .5.5 0 0 1 1 0 2 2 0 1 0 2-2H.5a.5.5 0 0 1-.5-.5z"/>
+      </svg> 
+      ${data.wind.speed}mph
+    </span>
+    </div>
+    `);
   });
 
   // TODO: Update your layout and AJAX request(s) to display a five day forecast for the city you live in that looks like the screenshot below.
@@ -12,47 +46,45 @@
   // TODO: If you want to add the icons the URLs for OpenWeatherMap's icons are formatted like: http://openweathermap.org/img/w/[icon].png where [icon] comes from the API response.
 
   // TODO: Refer to your Mapbox API exercise. Recreate the map below your 5 day forecast.
-  mapboxgl.accessToken = mapboxKey;
-  const map = new mapboxgl.Map({
-    container: "map",
-    style: "mapbox://styles/mapbox/streets-v12",
-    center: [-79.02119103975892, 35.12703524187906], // starting position [lng, lat]
-    zoom: 10, // starting zoom
-  });
 
-  $(() => {
-    $(`.zoom-in`).hover(
-      () => {
-        $(`[data-toggle="zoom-in"]`).popover("show");
-      },
-      () => {
-        $(`[data-toggle="zoom-in"]`).popover("hide");
-      }
-    );
+  // mapboxgl.accessToken = mapboxKey;
+  // const map = new mapboxgl.Map({
+  //   container: "map",
+  //   style: "mapbox://styles/mapbox/streets-v12",
+  //   center: [-79.02119103975892, 35.12703524187906], // starting position [lng, lat]
+  //   zoom: 10, // starting zoom
+  // });
 
-    $(`.zoom-out`).hover(
-      () => {
-        $('[data-toggle="zoom-out"]').popover(`show`);
-      },
-      () => {
-        $('[data-toggle="zoom-out"]').popover(`hide`);
-      }
-    );
-
-    $(`.remove-marker`).hover(
-      () => {
-        $('[data-toggle="remove-marker"]').popover(`show`);
-      },
-      () => {
-        $('[data-toggle="remove-marker"]').popover(`hide`);
-      }
-    );
-  });
-
+  // $(() => {
+  //   $(`.zoom-in`).hover(
+  //     () => {
+  //       $(`[data-toggle="zoom-in"]`).popover("show");
+  //     },
+  //     () => {
+  //       $(`[data-toggle="zoom-in"]`).popover("hide");
+  //     }
+  //   );
+  //   $(`.zoom-out`).hover(
+  //     () => {
+  //       $('[data-toggle="zoom-out"]').popover(`show`);
+  //     },
+  //     () => {
+  //       $('[data-toggle="zoom-out"]').popover(`hide`);
+  //     }
+  //   );
+  //   $(`.remove-marker`).hover(
+  //     () => {
+  //       $('[data-toggle="remove-marker"]').popover(`show`);
+  //     },
+  //     () => {
+  //       $('[data-toggle="remove-marker"]').popover(`hide`);
+  //     }
+  //   );
+  // });
 
   // Read through the documentation for the Mapbox API and figure out how to allow the user to drop a pin on any place on the map. Once the user drops a pin, grab its coordinates and feed those into your OpenWeatherMap API. Update the five-day forecast with the information from those coordinates (you should also get rid of your input boxes at this point).
 
   // TODO: Add a Mapbox text input to search by location and have the forecast update when a new location is searched.
 
-  // TODO: As a bonus make sure you can update the marker's position to the new search result.
+  // TODO: As a bonus make sure you can update the marker's position to the new search result.\
 })();
